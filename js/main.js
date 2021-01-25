7b5defd14f4945b5e0c4ad61fb033a08b049ded0
thamiriscardoso <thamiris.cj@aluno.ifsc.edu.br>
2021-01-25 00:19:58 -0300

    Conteudo aula 1

---
+++ js/main.js
@@ -0,0 +1,17 @@
+var nome= "Thamiris Cardoso";
+var idade = 28;
+var idade2 =10;
+var n1 = 28;
+var n2 =10;
+var frase = "Japão é o melhor time do mundo"
+
+//alert("Nome" + nome+" tem "+idade +" anos");
+//alert(idade + idade2);
+
+console.log(nome);
+console.log(idade+idade2);
+console.log(frase.toUpperCase());
+//console.log(frase.toLowerCase());
+//console.log(frase.replace("Japão","Brasil"));
+console.log(n1*n2);
+
